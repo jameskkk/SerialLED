fc017977d860f47f8f979def92b171caa8ce5a78
jameskkk <chenjhkk@gmail.com>
2020-11-30 14:52:24 +0800

    Update comment.

--- SerialLED.ino
+++ SerialLED.ino
@@ -1,21 +1,9 @@
 /*
-  Serial Event example
-
-  When new serial data arrives, this sketch adds it to a String.
-  When a newline is received, the loop prints the string and clears it.
-
-  A good test for this is to try it with a GPS receiver that sends out
-  NMEA 0183 sentences.
-
-  NOTE: The serialEvent() feature is not available on the Leonardo, Micro, or
-  other ATmega32U4 based boards.
-
-  created 9 May 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/SerialEvent
+  Arduino Serial LED Sample
+  Command:
+  START_BYTE   0x1B
+  TRIGGER_BYTE 0x31
+  STOP_BYTE    0x30
 */
 #include <Arduino.h>
 
